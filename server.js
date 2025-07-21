d8aeaaf90bd2faa56e43158cca442e58a1a816ec
Anuj Maurya <anuj24122001@gmail.com>
2025-07-21 23:54:34 +0530

    serving the frontend

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js';
+
+
+const PORT = process.env.PORT || 8000;
+
+const __dirname = import.meta.dirname
+
+const server = http.createServer(async (req, res)=> {
+
+    await serveStatic(req, res, __dirname)
+    // res.statusCode = 200
+    // res.setHeader('Content-Type', 'text/html')
+    // res.end('<h1>The server is working!</h1>');
+});
+
+
+server.listen(PORT, ()=> console.log(`Server is running at http://localhost:${PORT}`))
